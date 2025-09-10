20c63d5b0c6c9c0224c6cb94e784e709c4bbdb87
Mason duBoef <mduboef@gmail.com>
2025-09-10 14:06:36 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import csv
+import random
+import numpy as np
+from colorama import Fore
+
+from donor import Donor
+from agency import Agency, Preference
+from driver import Driver
+
+def main():
+
+    # read in agency data
+
+    # read in donor data
+
+    # populate adjancency matrix connecting agencies to donors if feasible
+    # TODO start by populating them at random
+
+    adjMatrix = np.zeros((10, 10))  # placeholder for actual size
+
+    #
+
+    return 0
