0f0762da53a493f27adcf9fc41b56d2792c6b1fe
Paula Navarrete Díaz <paula@paula.local>
2025-10-29 18:58:49 -0400

    created item class, separate from donor. item has attribute donor now

---
+++ item.py
@@ -0,0 +1,11 @@
+class Item:
+    def __init__(self, donor, weight, foodType):
+        self.donor = donor  # object from the class donor, corresponding to the donor that donated the item
+        self.weight = weight  # in pounds
+        self.foodType = foodType  # list of applicable food categories (legacy)
+        self.availabilityTime = None
+        self.probability = 1.0  # probability of availability
+
+        # new food type quantities for the updated formulation
+        # foodTypeQuantities[foodType] = pounds of that food type in this item
+        self.foodTypeQuantities = {}  # dict mapping food type to quantity in pounds
